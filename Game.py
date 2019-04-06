8c05336e9688ac93b2387f044b6d43c669939250
tours <tim_ours@taylor.edu>
2019-04-06 16:20:44 -0400

    Added a menu and broke things out of ManInTheVan.py.

---
+++ Game.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import os
+import sys
+import time
+
+import contextlib
+with contextlib.redirect_stdout(None): import pygame
+import pygame.gfxdraw
+
+from LevelLoader import loadLevel
+from Robber import Robber
+from Camera import Camera
+
+
+
+class Game():
+	def __init__(self, level):
+		self.rx, self.ry, self.robbermap, self.tilemap, self.cameras, self.robots, self.walls, self.floors = loadLevel(level)
+
+		self.robber = Robber(self.rx, self.ry)
+		self.robbers = pygame.sprite.Group()
+		self.robbers.add(self.robber)
+
+		self.selected = None
+		self.selectedRect = None
+
+	def updateSelected(self, event):
+		clickedOnObject = False
+		for object in self.cameras.sprites() + self.robots.sprites():
+			if object.rect.collidepoint(event.pos):
+				self.selectedRect = pygame.Rect(object.rect.x, object.rect.y, object.width, object.height)
+				self.selected = object
+				clickedOnObject = True
+			if not clickedOnObject and self.selected:
+				self.selected = True
+
+	def update(self, keys_pressed):
+		if self.selected:
+			self.selectedRect.x = self.selected.rect.x
+			self.selectedRect.y = self.selected.rect.y
+			self.selected.update(keys_pressed, self.tilemap)
+
+		self.robber.move(self.robbermap)
+		return -2
+
+	def draw(self, screen, surface):
+		self.floors.draw(surface)
+		self.robots.draw(surface)
+		self.robbers.draw(surface)
+		
+		for object in self.cameras.sprites():
+			if not object.isSeen((self.robber.rect.x + self.robber.rect.width / 2, self.robber.rect.y + self.robber.rect.height /2)):
+				pygame.gfxdraw.filled_polygon(screen, object.getLightCone(), pygame.Color(89, 211, 255, 50))
+			else:
+				pygame.gfxdraw.filled_polygon(screen, object.getLightCone(), pygame.Color(180, 0, 0, 50))
+		self.cameras.draw(surface)
+		self.walls.draw(surface)
+		
+		if self.selected:
+			pygame.gfxdraw.rectangle(screen, self.selectedRect, pygame.Color(255, 100, 16, 100))
+
